4f9def68ac6335b12f35bd195d85b264dda07ce2
Michael Bar-Sinai <mich.barsinai@gmail.com>
2016-05-15 21:00:11 +0300

    Added the LSC over BPjs project (4th incarnation of this :-/)

---
+++ LSCoverBPjs/src/lscoverbpjs/LSCoverBPjs.java
@@ -0,0 +1,19 @@
+/*
+ *  (C) Michael Bar-Sinai
+ */
+package lscoverbpjs;
+
+/**
+ *
+ * @author michael
+ */
+public class LSCoverBPjs {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}

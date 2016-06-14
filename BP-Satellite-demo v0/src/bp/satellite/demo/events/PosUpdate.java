e14eaa9e24beb27dff9253af54558069be56886a
geraw <geraw@seg123.wincs.cs.bgu.ac.il>
2016-06-14 17:28:41 +0300

    Software Update Button

--- BP-Satellite-demo v0/src/bp/satellite/demo/events/PosUpdate.java
+++ BP-Satellite-demo v0/src/bp/satellite/demo/events/PosUpdate.java
@@ -5,20 +5,19 @@ import bp.events.BEvent;
 /**
  * A class for Position Update .
  */
+public class PosUpdate extends BEvent {
 
-public class PosUpdate extends BEvent{
-	  public float SatPos;
-          public int SimTime;
+    public float SatPos;
+    public int SimTime;
 
-	  public PosUpdate(int SimTime, float SatPos) {
-	        super();
-                this.SimTime = SimTime;
-	        this.SatPos = SatPos;
-	    }
+    public PosUpdate(int SimTime, float SatPos) {
+        super();
+        this.SimTime = SimTime;
+        this.SatPos = SatPos;
+    }
 
-	  @Override
-	    public String toString() {
-	        return  "PosUpdate(" + SimTime + "," + SatPos  + ")";
-		  //return  "PosUpdate";
-	    }
+    @Override
+    public String toString() {
+        return "PosUpdate(" + SimTime + "," + SatPos + ")";
+    }
 }
--- BP-Satellite-demo v0/src/bp/satellite/demo/events/StaticEvents.java
+++ BP-Satellite-demo v0/src/bp/satellite/demo/events/StaticEvents.java
@@ -3,13 +3,11 @@ package bp.satellite.demo.events;
 import bp.events.BEvent;
 import bp.eventsets.EventsOfClass;
 
-
-
 public class StaticEvents {
+
     static public BEvent StartSimulation = new BEvent("StartSimulation");
-    static public BEvent Tick = new BEvent("Tick");
-    static public BEvent TakePicture = new BEvent("TakePicture");
-    
+    static public BEvent Tick            = new BEvent("Tick");
+    static public BEvent TakePicture     = new BEvent("TakePicture");
+
     public static final EventsOfClass PosUpdateEvent = new EventsOfClass(PosUpdate.class);
-    
 }
